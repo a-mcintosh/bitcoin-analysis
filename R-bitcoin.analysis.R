3119899578fd907607e11b0456d1ee279b33e932
Aubrey McIntosh, PhD <aubrey.mcintosh@utexas.edu>
2018-02-05 20:38:58 -0600

    Generates png file
    Signed-off-by: Aubrey McIntosh, PhD <aubrey.mcintosh@utexas.edu>

--- R-bitcoin.analysis.R
+++ R-bitcoin.analysis.R
@@ -1,71 +1,80 @@
 #  ------------------------------  ------------------------------
 #  -- Aubrey McIntosh, PhD  2018-02-04
-#  -- R code to pull in bitcoin csv file and create some plots
+#  -- R code to pull in a bitcoin csv file and create some plots
 #  -- This script and the .csv file could live in ~/projects/bitcoin-analysis
 #  ------------------------------  ------------------------------
 #  -- in R, type the command
 #  -- > source("~/projects/bitcoin-analysis/R-bitcoin.analysis.R")
 #  ------------------------------  ------------------------------
+
+#  ------------------------------  ------------------------------
+#  --  set up the environment
 bitcoin_lab <- read.csv("~/projects/bitcoin-analysis/bitcoin-lab-blocktimes.csv", header=F)
 colnames(bitcoin_lab) <- c("nHeight","MedianTimePrev","nTime", "calctime")
-# png("bitcoin-analysis.png")
-dev.new(width=16, height=10)
-par( mfrow=c(3,2))
+png("bitcoin-analysis.png", width=800, height=600)
+#dev.new()
+par( mfrow=c(2,2))
+
+#  ------------------------------  ------------------------------
+#  --  pre conditioning the data
+one_block_time <- 10 * 60	#A well known bitcoin number
+temp_vetted <- bitcoin_lab[ bitcoin_lab[0:10007,"calctime"] < 2*6*one_block_time, "nHeight" ]
+vetted <- temp_vetted[ temp_vetted < 10007 ]
+nblock <- length(vetted)
+one_block_time <- sum( bitcoin_lab[vetted,"calctime"]) / nblock / 6
+one_block_time
 
 #  ------------------------------  ------------------------------
 #  --  Histogram of MedianPrevTime
-present<-c(2:14,21:27,34:10007)
 lambda <- 12
-x <- c(0:30)
+x <- c(0:29)
 y <- dpois( x, lambda)
-nblock <- length(present)
-plot( x, nblock*y, col="black", pch=3, ylab="Block Count", xlab="MedianTimePast (half-block units)", ylim=c(0,0.15*nblock));
+y_lim <- ceiling(nblock*max(y)/0.8)
 
-one_block <- (bitcoin_lab[10007,"nTime"]-bitcoin_lab[2,"nTime"]) / ( 10007 - 2)
-breaks <- seq(0, 16*one_block, one_block/2)
-points( type="p", table( cut( bitcoin_lab[present,"calctime"], breaks, right=FALSE)), col="black", ylim=c(0,1250), pch=0)
+plot( 1+x, nblock*y, col="black", pch=3, ylab="Block Count", xlab="MedianTimePast (half-block units)", ylim=c(0, y_lim));
+
+breaks <- seq(0, 15*one_block_time, one_block_time/2)
+points( type="p", table( cut( bitcoin_lab[vetted,"calctime"], breaks, right=FALSE)), col="black", pch=0)
 legend( "topright", pch=c(0,3), title="Source of Data", c("blockchain", "Poisson calculation"))
-title(main="Early Bitcoin Statistics", sub=paste("Selected", toString(nblock), "Blocks"))
+title(main="Early Bitcoin Statistics", sub=paste("vetted", toString(nblock), "Blocks"))
 abline( v=lambda, col="red" )
-abline( v=2*lambda, col="green" )
 
 #  ------------------------------  ------------------------------
 #  -- graph MedianPrevTime vs nHeight
-plot(bitcoin_lab[present,"nHeight"], bitcoin_lab[present,"calctime"], ylab="MedianTimePast", xlab="nBlock", pch=4, cex=0.25)
-abline( h=lambda / 2 * one_block, col="red" )
-abline( h=lambda     * one_block, col="green" )
-title(main="MedianTimePast", sub=paste("Blocktime", toString(one_block), "seconds"))
+plot(bitcoin_lab[vetted,"nHeight"], bitcoin_lab[vetted,"calctime"], ylab="MedianTimePast", xlab="nBlock", pch=4, cex=0.25)
+abline( h=lambda / 2 * one_block_time, col="red" )
+title(main="MedianTimePast", sub=paste("Blocktime", toString(one_block_time), "seconds"))
 
-present<-c(1387:1928)
-points(bitcoin_lab[present,"nHeight"], bitcoin_lab[present,"calctime"], pch=4, cex=0.25, col="blue")
+anomoly<-c(1387:1928)
+points(bitcoin_lab[anomoly,"nHeight"], bitcoin_lab[anomoly,"calctime"], pch=4, cex=0.25, col="blue")
 
 #  ------------------------------  ------------------------------
-plot( x, nblock*y, col="black", pch=3, ylab="Block Count", xlab="MedianTimePast (selected)", ylim=c(0,0.15*nblock));
-points( type="p", table( cut( bitcoin_lab[present,"calctime"], breaks, right=FALSE)), col="blue", ylim=c(0,1250), pch=0)
+#  --  Histogram of MedianPrevTime anomoly nHeight 1387:1928
+
+plot( 1+x, nblock*y, col="black", pch=3, ylab="Block Count", xlab="MedianTimePast (vetted)", ylim=c(0,y_lim));
+points( table( cut( bitcoin_lab[anomoly,"calctime"], breaks, right=FALSE)), type="p", col="blue", ylim=c(0,1250), pch=0)
 legend( "topright", pch=c(0,3), title="Source of Data", c("blockchain 1387-1928", "Poisson calculation"))
 title(main="Poisson Descrepancy Highlighted", sub=paste("First", "1387-1928", "Blocks"))
 abline( v=lambda, col="red" )
-abline( v=2*lambda, col="green" )
 
 
 #  ------------------------------  ------------------------------
-present<-c(1387:1928)
-plot(bitcoin_lab[present,"nHeight"], bitcoin_lab[present,"calctime"], ylab="MedianTimePast", xlab="nBlock", pch=4, cex=0.25, col="blue")
-abline( h=lambda / 2 * one_block, col="red" )
+#  -- graph MedianPrevTime vs nHeight anomoly
+vetted<-c(1387:1928)
+plot(bitcoin_lab[vetted,"nHeight"], bitcoin_lab[vetted,"calctime"], ylab="MedianTimePast", xlab="nBlock", pch=4, cex=0.25, col="blue")
+points( table( cut( bitcoin_lab[anomoly,"calctime"], breaks, right=FALSE)), type="p", col="blue", ylim=c(0,1250), pch=0)
+abline( h=lambda / 2 * one_block_time, col="red" )
 title(main="Highlighted MedianTimePast", sub=paste("Fast Miners"))
+vetted <- old_vetted
 
 
 #  ------------------------------  ------------------------------
-#  notes that don't really work.
-#  intended for a data-vetting operation
+#  Workbench
 
-bitcoin_lab[1:10,"calctime"] < 2000
-present <- bitcoin_lab[0:10008,"calctime"] < 29 * one_block
-present <- bitcoin_lab[ bitcoin_lab[0:10008,"calctime"] < 20000, "nHeight" ]
-present <- bitcoin_lab[ bitcoin_lab[0:10008,"calctime"] > 29*one_block, "nHeight" ]
+#  cleaned before this commit
+
 #  ------------------------------  ------------------------------
-#  ------------------------------  ------------------------------
-# dev.off()
+dev.off()
 
 
 
