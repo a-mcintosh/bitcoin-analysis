f125dae691369c16bb3ff1286580ccf8dd772a04
Aubrey McIntosh, PhD <aubrey.mcintosh@utexas.edu>
2018-02-05 21:49:05 -0600

    Fixed display alignment with axis label.  Discovered bug if display range gets large, e.g. c(0:15000) the excessively large calctime values are not ignored.
    Signed-off-by: Aubrey McIntosh, PhD <aubrey.mcintosh@utexas.edu>

--- R-bitcoin.analysis.R
+++ R-bitcoin.analysis.R
@@ -8,6 +8,12 @@
 #  ------------------------------  ------------------------------
 
 #  ------------------------------  ------------------------------
+#  --  N.B.  an R table is indexed from "1" and the Poisson distribution starts at "0"
+#  --  The limits in the code were chosen so that the Poisson bin numbers are correct, and
+#  --  the R histogram correctly lines up with the Poisson calculation
+#  ------------------------------  ------------------------------
+
+#  ------------------------------  ------------------------------
 #  --  set up the environment
 bitcoin_lab <- read.csv("~/projects/bitcoin-analysis/bitcoin-lab-blocktimes.csv", header=F)
 colnames(bitcoin_lab) <- c("nHeight","MedianTimePrev","nTime", "calctime")
@@ -17,23 +23,23 @@ par( mfrow=c(2,2))
 
 #  ------------------------------  ------------------------------
 #  --  pre conditioning the data
-one_block_time <- 10 * 60	#A well known bitcoin number
-temp_vetted <- bitcoin_lab[ bitcoin_lab[0:10007,"calctime"] < 2*6*one_block_time, "nHeight" ]
-vetted <- temp_vetted[ temp_vetted < 10007 ]
+one_block_time <- 10 * 60	#A well known bitcoin number.  10 min/block * 60 sec/min
+initial_range=c(0:10007)
+temp_vetted <- bitcoin_lab[ bitcoin_lab[initial_range,"calctime"] < (2.5 * 6 * one_block_time), "nHeight" ]
+vetted <- temp_vetted[ temp_vetted < (length(initial_range)-1) ]
 nblock <- length(vetted)
 one_block_time <- sum( bitcoin_lab[vetted,"calctime"]) / nblock / 6
-one_block_time
 
 #  ------------------------------  ------------------------------
 #  --  Histogram of MedianPrevTime
 lambda <- 12
-x <- c(0:29)
+x <- c(1:29)
 y <- dpois( x, lambda)
 y_lim <- ceiling(nblock*max(y)/0.8)
 
-plot( 1+x, nblock*y, col="black", pch=3, ylab="Block Count", xlab="MedianTimePast (half-block units)", ylim=c(0, y_lim));
+plot( x, nblock*y, col="black", pch=3, ylab="Block Count", xlab="MedianTimePast (half-block units)", ylim=c(0, y_lim));
 
-breaks <- seq(0, 15*one_block_time, one_block_time/2)
+breaks <- seq(one_block_time/2, 15*one_block_time, one_block_time/2)
 points( type="p", table( cut( bitcoin_lab[vetted,"calctime"], breaks, right=FALSE)), col="black", pch=0)
 legend( "topright", pch=c(0,3), title="Source of Data", c("blockchain", "Poisson calculation"))
 title(main="Early Bitcoin Statistics", sub=paste("vetted", toString(nblock), "Blocks"))
@@ -51,7 +57,7 @@ points(bitcoin_lab[anomoly,"nHeight"], bitcoin_lab[anomoly,"calctime"], pch=4, c
 #  ------------------------------  ------------------------------
 #  --  Histogram of MedianPrevTime anomoly nHeight 1387:1928
 
-plot( 1+x, nblock*y, col="black", pch=3, ylab="Block Count", xlab="MedianTimePast (vetted)", ylim=c(0,y_lim));
+plot( x, nblock*y, col="black", pch=3, ylab="Block Count", xlab="MedianTimePast (vetted)", ylim=c(0,y_lim));
 points( table( cut( bitcoin_lab[anomoly,"calctime"], breaks, right=FALSE)), type="p", col="blue", ylim=c(0,1250), pch=0)
 legend( "topright", pch=c(0,3), title="Source of Data", c("blockchain 1387-1928", "Poisson calculation"))
 title(main="Poisson Descrepancy Highlighted", sub=paste("First", "1387-1928", "Blocks"))
@@ -60,6 +66,7 @@ abline( v=lambda, col="red" )
 
 #  ------------------------------  ------------------------------
 #  -- graph MedianPrevTime vs nHeight anomoly
+old_vetted <- vetted
 vetted<-c(1387:1928)
 plot(bitcoin_lab[vetted,"nHeight"], bitcoin_lab[vetted,"calctime"], ylab="MedianTimePast", xlab="nBlock", pch=4, cex=0.25, col="blue")
 points( table( cut( bitcoin_lab[anomoly,"calctime"], breaks, right=FALSE)), type="p", col="blue", ylim=c(0,1250), pch=0)
